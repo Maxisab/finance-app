feb94959524c916e242fa0e1725fdc44fb428ef0
Max Sabate <max.sabate@gmail.com>
2022-06-21 14:28:08 -0700

    react router installed/routes set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// components
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+
+
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/Login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
