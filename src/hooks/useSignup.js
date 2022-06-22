9c7be244d60aa0682b00cf3f23efaf8e5b5a576a
Max Sabate <max.sabate@gmail.com>
2022-06-22 13:34:01 -0700

    fixed cleanup func and added some login func

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -27,14 +27,14 @@ export const useSignup = () => {
       dispatch({ type: 'LOGIN', payload: res.user })
 
       // update state
-      if (isCancelled) {
+      if (!isCancelled) {
         setIsPending(false)
         setError(null)
       }
 
     }
     catch (err) {
-      if (isCancelled) {
+      if (!isCancelled) {
         console.log(err.message)
         setError(err.message)
         setIsPending(false)
